ef68ab337f84cb24ad5221d759196c6999ac73c5
Victor Jonhson Aciole do Nascimento <101227209+VJonhson-webDeveloper@users.noreply.github.com>
2022-10-14 13:17:35 -0300

    first commit

---
+++ app/controllers/TelaInicial.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import play.mvc.Controller;
+
+
+public class TelaInicial extends Controller {
+
+    public static void inicio() {
+        render();
+    }
+    
+}
---
+++ app/controllers/Usuarios.java
@@ -0,0 +1,48 @@
+package controllers;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.hibernate.mapping.Collection;
+
+import models.Usuario;
+import play.mvc.Controller;
+import play.mvc.results.RenderTemplate;
+
+public class Usuarios extends Controller {
+
+    public static void forms() {
+        render();
+    }
+
+    public static void salvar(Usuario u) {
+        u.save();
+        listar();
+    }
+
+    public static void listar() {
+        String termo = params.get("termo");
+		
+		List<Usuario> usuarios = Collections.EMPTY_LIST;
+		if (termo == null || termo.isEmpty()) {
+			usuarios = Usuario.findAll();
+		} else {
+			usuarios = Usuario.find("(lower(nome) like ?1 OR cpf like ?2)", 
+					"%" + termo.toLowerCase() + "%",
+					"%" + termo.toLowerCase() + "%").fetch();
+		}
+		render(usuarios, termo);
+    }
+
+    public static void remover(Long id) {
+        Usuario u = Usuario.findById(id);
+        u.delete();
+        listar();
+    }
+
+    public static void editar(Long id) {
+        Usuario u = Usuario.findById(id);
+        renderTemplate("Usuarios/forms.html", u);
+    }
+    
+}
