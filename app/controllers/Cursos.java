0e61f6e8d8b89366bb6a5cfa12aa10d79e4bb9b8
VJonhson-webDeveloper <v.nascimento@escolar.ifrn.edu.br>
2022-11-16 16:11:30 -0300

    Adicionando CRUD a cursos e ajustes de CSS.

---
+++ app/controllers/Cursos.java
@@ -0,0 +1,10 @@
+package controllers;
+
+import play.mvc.Controller;
+
+public class Cursos extends Controller {
+    
+    public static void forms() {
+        render();
+    }
+}
---
+++ app/models/Curso.java
@@ -0,0 +1,14 @@
+package models;
+
+import javax.persistence.Entity;
+
+import play.db.jpa.Model;
+
+@Entity
+public class Curso extends Model {
+
+    public String titulo;
+    public String descricao;
+    public String foto;
+    
+}
