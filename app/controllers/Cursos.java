fc761b9df25daf1b7e07502a3d55c3b939f0909e
VJonhson-webDeveloper <v.nascimento@escolar.ifrn.edu.br>
2022-11-23 08:15:53 -0300

    Adicionando métodos de Listagem, edição e remoção de Cursos.

--- app/controllers/Cursos.java
+++ app/controllers/Cursos.java
@@ -1,10 +1,34 @@
 package controllers;
 
+import java.util.List;
+
+import models.Curso;
 import play.mvc.Controller;
 
 public class Cursos extends Controller {
     
     public static void forms() {
         render();
+    }
+
+    public static void salvar(Curso curso) {
+        curso.save();
+        listar();
+    }
+
+    public static void listar() {
+        List<Curso> cursos = Curso.findAll();
+        render(cursos);
+    }
+
+    public static void remover(Long idCurso) {
+        Curso c = Curso.findById(idCurso);
+        c.delete();
+        listar();
+    }
+
+    public static void editar(Long idCurso) {
+        Curso c = Curso.findById(idCurso);
+        renderTemplate("Cursos/forms.html", c);
     }
 }
