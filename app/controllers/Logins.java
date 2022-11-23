4439e6fae645f548ae954e1093839e730c4decbf
VJonhson-webDeveloper <v.nascimento@escolar.ifrn.edu.br>
2022-11-23 16:10:35 -0300

    Método para autenticar os usuários.

--- app/controllers/Logins.java
+++ app/controllers/Logins.java
@@ -1,10 +1,31 @@
 package controllers;
 
+import models.Usuario;
 import play.mvc.Controller;
 
 public class Logins extends Controller {
 
     public static void login() {
         render();
+    }
+
+    public static void logar(String email, String senha) {
+        Usuario usuario = Usuario.find("login = ?1 and senha = ?2", email, senha).first();
+
+        if (usuario == null) {
+            flash.error("Email ou senha inválidos");
+            login();
+
+        } else {
+            session.put("usuario", usuario.email);
+            Usuarios.listar();
+        }
+
+    }
+
+    public static void logout() {
+        session.clear();
+        flash.success("Você saiu do sistema!");
+        login();
     }
 }
