4c24c0d03d47a52c2577361440dd5cfac5fe201e
VJonhson-webDeveloper <v.nascimento@escolar.ifrn.edu.br>
2022-12-07 15:02:25 -0300

    Sistema de Login

--- app/controllers/Seguranca.java
+++ app/controllers/Seguranca.java
@@ -7,7 +7,7 @@ import play.mvc.Controller;
 public class Seguranca extends Controller {
     
     @Before
-    static void autenticarUsuarios() {
+    static void verificar() {
 
         if (session.contains("usuario.email") == false ) {
             Logins.login();
