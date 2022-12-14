7c8fb9f445f202da7ee7b54e856877e5266fdf1d
VJonhson-webDeveloper <v.nascimento@escolar.ifrn.edu.br>
2022-12-14 10:17:06 -0300

    Adicionando exclusão lógica, mapeamento OR.

--- app/controllers/Usuarios.java
+++ app/controllers/Usuarios.java
@@ -5,12 +5,13 @@ import java.util.List;
 
 import org.hibernate.mapping.Collection;
 
+import models.Status;
 import models.Usuario;
 import play.mvc.Controller;
 import play.mvc.With;
 import play.mvc.results.RenderTemplate;
 
-@With(Seguranca.class)
+
 public class Usuarios extends Controller {
 
     public static void forms() {
@@ -36,17 +37,19 @@ public class Usuarios extends Controller {
 		
 		List<Usuario> usuarios = Collections.EMPTY_LIST;
 		if (termo == null || termo.isEmpty()) {
-			usuarios = Usuario.findAll();
+			usuarios = Usuario.find("status = ?1", Status.ATIVO).fetch();
 		} else {
-			usuarios = Usuario.find("(lower(nome) like ?1 OR cpf like ?2)", 
+			usuarios = Usuario.find("(lower(nome) like ?1 OR cpf like ?2) AND status = ?3", 
 					"%" + termo.toLowerCase() + "%",
-					"%" + termo.toLowerCase() + "%").fetch();
+					"%" + termo.toLowerCase() + "%",
+                    Status.ATIVO).fetch();
 		}
 		render(usuarios, termo);
     }
 
     public static void remover(Long id) {
         Usuario u = Usuario.findById(id);
+        u.status = Status.INAVTIVO;
         u.save();
         listar();
     }
